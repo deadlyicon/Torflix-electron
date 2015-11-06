5c26521732f69777d17d7e85e3829a4e8de49738
Jared Grippe <jared@devbootcamp.com>
2015-11-06 14:34:48 -0500

    WIP

--- main.js
+++ main.js
@@ -4,17 +4,13 @@ var BrowserWindow = require('browser-window');  // Module to create native brows
 // Report crashes to our server.
 require('crash-reporter').start();
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-var mainWindow = null;
-
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform != 'darwin') {
-    app.quit();
-  }
+  // // On OS X it is common for applications and their menu bar
+  // // to stay active until the user quits explicitly with Cmd + Q
+  // if (process.platform != 'darwin') {
+  //   app.quit();
+  // }
 });
 
 // This method will be called when Electron has finished
