1f499bbf69e98b6f4a4fcacefec7db72a78a7ffa
Jared Grippe <jared@deadlyicon.com>
2015-11-22 00:12:54 -0800

    WIP

--- render.js
+++
@@ -1,9 +0,0 @@
-// var jQuery = require('./bower_components/jquery/dist/jquery')
-// var Page = require('./render/Page')
-
-// debugger
-
-// setTimeout(()=>
-//   ReactDOM.render(<Page />, document.body.children[0])
-// );
-
